06904cda948d9342bbfa2bffaaf1ad5b53f2b212
limingming <limingming@wident.cn>
2018-04-19 18:34:28 +0800

    change

--- handle.go
+++ handle.go
@@ -30,7 +30,7 @@ func (pxy *proxy) handleStatus(p *p2p.Peer, msg p2p.Msg, rw p2p.MsgReadWriter) (
 	pxy.upstreamConn[p.ID()] = &conn{p, rw}
 	// pxy.allPeer[p.ID()] = p
 	// NewPeer(version int, p *p2p.Peer, rw p2p.MsgReadWriter)
-	pp := ethpeer.NewPeer(myMessage.ProtocolVersion, p, rw)
+	pp := ethpeer.NewPeer(int(myMessage.ProtocolVersion), p, rw)
 	pxy.ethpeerset.Register(pp)
 	pxy.lock.Unlock()
 	logger.Info("add:", p.ID())
