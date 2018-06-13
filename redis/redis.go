2d693abeb48670437de5745a1fc984445697d892
lzxm160 <lzxm160@sina.com>
2018-06-13 22:27:30 +0800

    start hacking

--- redis/redis.go
+++ redis/redis.go
@@ -48,7 +48,37 @@ func (b *BlockData) key() string {
 //
 //	return &RedisClient{client: client, prefix: prefix}
 //}
-
+func join(args ...interface{}) string {
+	s := make([]string, len(args))
+	for i, v := range args {
+		switch v.(type) {
+		case string:
+			s[i] = v.(string)
+		case int64:
+			s[i] = strconv.FormatInt(v.(int64), 10)
+		case uint64:
+			s[i] = strconv.FormatUint(v.(uint64), 10)
+		case float64:
+			s[i] = strconv.FormatFloat(v.(float64), 'f', 0, 64)
+		case bool:
+			if v.(bool) {
+				s[i] = "1"
+			} else {
+				s[i] = "0"
+			}
+		case *big.Int:
+			n := v.(*big.Int)
+			if n != nil {
+				s[i] = n.String()
+			} else {
+				s[i] = "0"
+			}
+		default:
+			panic("Invalid type specified for conversion")
+		}
+	}
+	return strings.Join(s, ":")
+}
 func NewRedisClient(cfg *Config, prefix string) *RedisClient {
 	client := redis.NewClient(&redis.Options{
 		Addr:     cfg.Endpoint,
