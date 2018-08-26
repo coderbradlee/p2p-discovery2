ed401e3937a9067fbea8be42d53016f3aa25d66b
lzxm160 <lzxm160@sina.com>
2018-08-26 18:19:16 +0800

    ethclient

--- ethclient/ethclient.go
+++ ethclient/ethclient.go
@@ -28,7 +28,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/rlp"
+	// "github.com/ethereum/go-ethereum/rlp"
 	"github.com/ethereum/go-ethereum/rpc"
 	"log"
 	"math/big"
